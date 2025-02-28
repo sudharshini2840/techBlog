e4f782a75cf6cd3e34a4b6dc5b57083e8e10f1d0
Sudharshini V <sudharshini.10712840@ltimindtree.com>
2025-02-28 19:34:18 +0530

    BaseProject

---
+++ src/test/java/com/src/techBlog/TechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.techBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
